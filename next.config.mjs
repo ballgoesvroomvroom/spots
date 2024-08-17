26547475a770e9146086ad199cde788d74e5b390
jayden <chongjayden07@gmail.com>
2024-08-17 17:57:09 +0800

    exposed clientId

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,8 @@
 const nextConfig = {
 	reactStrictMode: false,
 	env: {
-		ENV_MODE: process.env.ENV_MODE
+		ENV_MODE: process.env.ENV_MODE,
+		SPOTIFY_CLIENT_ID: process.env.SPOTIFY_CLIENT_ID
 	}
 };
 
--- src/app/login/spotify-client-wrapper.ts
+++ src/app/login/spotify-client-wrapper.ts
@@ -1,6 +1,6 @@
 "use client"
 
-export const clientId: string = "84dedfb61b7b47f68326212f81c4f155";
+export const clientId: string = process.env.SPOTIFY_CLIENT_ID;
 export const redirectURL: string = `${process.env.ENV_MODE === "dev" ? "http://localhost:3000/" : "https://spots-six.vercel.app/"}login/callback`
 const scope: string = "user-read-private user-read-email user-top-read"
 
